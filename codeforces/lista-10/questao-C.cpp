0ec68c7ca6e6811cc6b468ff0dc731c527504bee
Ester Oliveira <oliver.ester.2004@gmail.com>
2023-07-13 18:55:07 -0300

    Questão C. Tentando uma lógica para caso apareça outra sequência.

--- codeforces/lista-10/questao-C.cpp
+++ codeforces/lista-10/questao-C.cpp
@@ -3,28 +3,33 @@
 using namespace std;
 
 int main(){
-    string nome;
-    cin >> nome;
-    int a, c, g, t, letras;
-    letras = nome.size();
+    string tamanho;
+    cin >> tamanho;
+    
+    char nome[tamanho.size()];
+
+    for (int j=0; j<tamanho.size(); j++){
+        nome[j] = tamanho[j];
+    }
+
+    int a, c, g, t;
     a = 1;
     c = 1;
     g = 1;
     t = 1;
+    
+    for (int i=1; i<=tamanho.size(); i++){
+        if (nome[i] == 'A' && nome[i-1] != 'A' ) a = 1;
+        if (nome[i-1] == 'A' && nome[i] == 'A') a = a + 1;
 
-    for (int i=0; i<letras; i++){
-        if (nome[i] == 'A' && nome[i] == nome[i+1]){
-            a = a + 1;
-        }
-        else if (nome[i] == 'C' && nome[i] == nome[i+1]){
-            c = c + 1;
-        }
-        else if (nome[i] == 'G' && nome[i] == nome[i+1]){
-            g = g + 1;
-        }
-        else if (nome[i] == 'T' && nome[i] == nome[i+1]){
-            t = t + 1;
-        }
+        if (nome[i] == 'C' && nome[i-1] != 'C') c = 1;
+        if (nome[i-1] == 'C' && nome[i] == 'C') c = c + 1;
+        
+        if (nome[i] == 'G' && nome[i-1] != 'G') g = 1;
+        if (nome[i-1] == 'G' && nome[i] == 'G') g = g + 1;
+
+        if (nome[i] == 'T' && nome[i-1] != 'T') t = 1;
+        if (nome[i-1] == 'T' && nome[i] == 'T') t = t + 1;
     }
 
     int repeticoes[] = {a, c, g, t}, maior;
