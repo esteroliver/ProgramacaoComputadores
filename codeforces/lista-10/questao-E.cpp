67347b19355ab11dc020850a51163e31ac9ed90d
Ester Oliveira <oliver.ester.2004@gmail.com>
2023-07-26 10:47:03 -0300

    Update questao-E.cpp

--- codeforces/lista-10/questao-E.cpp
+++ codeforces/lista-10/questao-E.cpp
@@ -3,33 +3,39 @@
 using namespace std;
 
 int main(){
-        int num;
-        cin >> num;
+    int num;
+    while(cin >> num){
         int numeros[num];
-        
+            
         for(int i=0; i<num; i++){
             cin >> numeros[i];
         }
-        
-        int sequencia[num-1];
-        
+        int indice = num-1;
+        int sequencia[indice];
+            
         for (int j=1;j<num;j++){
-            sequencia[j-1] = (numeros[j] - numeros[j-1]);
-            if (sequencia[j-1] < 0) sequencia[j-1] = sequencia[j-1] * -1;
+            sequencia[j-1] = j;
         }
-        
-        int seq = 1;
-        
-        for (int k=0; k<(num-1);k++){
-            if ((sequencia[k] + 1) == sequencia[k+1] || (sequencia[k] - 1) == sequencia[k+1]){
-                seq = seq + 1;
+            
+        int seq = 0;
+
+        for (int k=0; k<(num-2);k++){
+            for (int l=1; l<num; l++){
+                int numero = numeros[l] - numeros[l-1];
+                if (numero < 0){
+                    numero = numero * -1;
+                }
+                if (sequencia[k] == numero) seq = seq + 1;
             }
+            
         }
-        
+
+        cout << seq << '\n';
         if(seq == num-1 || num == 1){
             cout << "Alegre" << '\n';
         }
         else{
             cout << "Nao alegre" << '\n';
-            }
+        }
+    }
 }
