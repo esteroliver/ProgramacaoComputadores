44c6030d20806db98694a3b933f129723515b11c
Ester Oliveira <oliver.ester.2004@gmail.com>
2023-07-26 11:35:12 -0300

    Questão E (resposta errada no teste 4)

--- codeforces/lista-10/questao-E.cpp
+++ codeforces/lista-10/questao-E.cpp
@@ -6,31 +6,32 @@ int main(){
     int num;
     while(cin >> num){
         int numeros[num];
-            
         for(int i=0; i<num; i++){
             cin >> numeros[i];
         }
+      
         int indice = num-1;
         int sequencia[indice];
-            
         for (int j=1;j<num;j++){
             sequencia[j-1] = j;
         }
             
         int seq = 0;
 
-        for (int k=0; k<(num-2);k++){
+        for (int k=0; k<=(num-2);k++){
             for (int l=1; l<num; l++){
                 int numero = numeros[l] - numeros[l-1];
                 if (numero < 0){
                     numero = numero * -1;
                 }
-                if (sequencia[k] == numero) seq = seq + 1;
+                if (sequencia[k] == numero){
+                  seq = seq + 1;
+                  sequencia[k] = 0;
+                };
             }
             
         }
-
-        cout << seq << '\n';
+      
         if(seq == num-1 || num == 1){
             cout << "Alegre" << '\n';
         }
